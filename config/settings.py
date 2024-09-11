82564f1cf051cd053ae21eed37a2efb0b0a2666f
Tokimasa Koda <tokihide121062@gmail.com>
2024-09-12 01:23:15 +0900

    setting update

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  #追記
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
