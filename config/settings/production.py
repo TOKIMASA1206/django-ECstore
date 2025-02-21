9a9f75d00ccfb9ab161b5671dba278a07de32a0c
TOKIMASA1206 <149468158+TOKIMASA1206@users.noreply.github.com>
2025-02-21 14:12:59 +0000

    環境変数の修正

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,17 +3,21 @@
 from .base import *
 from decouple import Config, RepositoryEnv
 import dj_database_url
-import django_heroku
 
-# .env.prod ファイルを指定して読み込む
-env_path = BASE_DIR / 'secrets' / '.env.prod'
-config = Config(RepositoryEnv(str(env_path)))
+# # .env.prod ファイルを指定して読み込む
+# env_path = BASE_DIR / 'secrets' / '.env.prod'
+# config = Config(RepositoryEnv(str(env_path)))
 
-# 環境変数の取得
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-STRIPE_API_SECRET_KEY = config('STRIPE_API_SECRET_KEY')
-MY_URL = config('MY_URL')
+# # 環境変数の取得
+# SECRET_KEY = config('SECRET_KEY')
+# DEBUG = config('DEBUG', default=False, cast=bool)
+# STRIPE_API_SECRET_KEY = config('STRIPE_API_SECRET_KEY')
+# MY_URL = config('MY_URL')
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+STRIPE_API_SECRET_KEY = os.environ.get('STRIPE_API_SECRET_KEY')
+MY_URL = os.environ.get('MY_URL')
 
 # データベース設定（Heroku の PostgreSQL を使用）
 DATABASES = {
@@ -24,8 +28,7 @@ DATABASES = {
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Heroku の設定
-django_heroku.settings(locals())
+
 
 # セキュリティ設定
 SECURE_SSL_REDIRECT = True
